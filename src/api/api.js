0901ad4cd4a2035846261c3747f6a4496faa2b0a
Serhii Babii <babiysergey1988@gmail.com>
2019-08-30 16:00:25 +0300

    Initial

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "b1a938ee7f324798663cd0727c5783ee";
+
+export const API_KEY_4 =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMWE5MzhlZTdmMzI0Nzk4NjYzY2QwNzI3YzU3ODNlZSIsInN1YiI6IjVkNjNmMzQ1YmM4YWJjNjA1ZDI1MDY5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0AmtMmTcqQ9lyLuhMfOXywHfhxLQ8NH110DBfcawH4c";
