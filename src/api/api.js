fb41f5712f0864be3a86cab5d682d759bcd8df52
Serhii Babii <babiysergey1988@gmail.com>
2019-09-16 00:29:36 +0300

    Added class CallApi

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+import queryString from "query-string";
+
 export const API_URL = "https://api.themoviedb.org/3";
 
 export const API_KEY_3 = "b1a938ee7f324798663cd0727c5783ee";
@@ -25,3 +27,76 @@ export const fetchApi = (url, option = {}) => {
       });
   });
 };
+
+export default class CallApi {
+  static get(url, options = {}) {
+    const { params = {} } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      ...params
+    };
+    return fetchApi(
+      `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+      {
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
+    );
+  }
+  static post(url, options = {}) {
+    const { params = {}, body } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      ...params
+    };
+    return fetchApi(
+      `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(body)
+      }
+    );
+  }
+  static mark(url, options = {}) {
+    const { params = {}, body } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      ...params
+    };
+    return fetchApi(
+      `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(body)
+      }
+    );
+  }
+  static delete(url, options = {}) {
+    const { params = {} } = options;
+    const queryStringParams = {
+      api_key: API_KEY_3,
+      ...params
+    };
+    return fetchApi(
+      `${API_URL}${url}?${queryString.stringify(queryStringParams)}`,
+      {
+        method: "DELETE",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json"
+        }
+      }
+    );
+  }
+}
