60e1835d88b920891c970852fc3c655922d5520c
Serhii Babii <babiysergey1988@gmail.com>
2019-09-25 23:17:17 +0300

    Changed component MovieTabs.js with Routes and Links

--- src/components/Pages/MoviePage/MovieTabs.js
+++ src/components/Pages/MoviePage/MovieTabs.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import Credits from "./Credits/Credits";
-import Video from "./Video/Video";
+import { Switch, Route } from "react-router";
+import { NavLink } from "react-router-dom";
+import Credits from "./MovieCredits/MovieCredits";
+import Video from "./MovieVideos/MovieVideos";
+import Detail from "./MovieDetail/MovieDetail";
+import NoMatch from "./NoMatch/NoMatch";
+import { Nav, NavItem } from "reactstrap";
 
-import {
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink,
-  Row,
-  Col,
-  Table
-} from "reactstrap";
 import classnames from "classnames";
 
 export default class MovieTabs extends Component {
@@ -28,27 +23,14 @@ export default class MovieTabs extends Component {
   };
 
   render() {
-    const {
-      id,
-      status,
-      release_date,
-      runtime,
-      budget,
-      revenue,
-      original_language,
-      genres,
-      production_countries,
-      production_companies
-    } = this.props.movie;
-
-    const { activeTab } = this.state;
-
+    const { movie, activeTab } = this.props;
     return (
       <div>
         <Nav tabs>
           <NavItem>
             <NavLink
-              className={classnames({ active: activeTab === "1" })}
+              to={`/movie/${movie.id}/detail`}
+              className={classnames("nav-link", { active: activeTab === "1" })}
               onClick={() => {
                 this.toggle("1");
               }}
@@ -58,7 +40,8 @@ export default class MovieTabs extends Component {
           </NavItem>
           <NavItem>
             <NavLink
-              className={classnames({ active: activeTab === "2" })}
+              to={`/movie/${movie.id}/videos`}
+              className={classnames("nav-link", { active: activeTab === "2" })}
               onClick={() => {
                 this.toggle("2");
               }}
@@ -68,7 +51,8 @@ export default class MovieTabs extends Component {
           </NavItem>
           <NavItem>
             <NavLink
-              className={classnames({ active: activeTab === "3" })}
+              to={`/movie/${movie.id}/credits`}
+              className={classnames("nav-link", { active: activeTab === "3" })}
               onClick={() => {
                 this.toggle("3");
               }}
@@ -77,83 +61,12 @@ export default class MovieTabs extends Component {
             </NavLink>
           </NavItem>
         </Nav>
-        <TabContent activeTab={activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col className="mt-4">
-                <Table>
-                  <tbody>
-                    <tr>
-                      <th scope="row">Статус:</th>
-                      <td>{status}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Дата выхода:</th>
-                      <td>{release_date}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Продолжительность:</th>
-                      <td>{runtime} мин</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Язык оригинала:</th>
-                      <td>{original_language}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Страна:</th>
-                      <td>
-                        {production_countries
-                          ? production_countries.map(item => {
-                              return item.name;
-                            })
-                          : null}
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Бюджет:</th>
-                      <td>{budget}$</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Сборы:</th>
-                      <td>{revenue}$</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Компания:</th>
-                      <td>
-                        {production_companies
-                          ? production_companies.map(item => {
-                              return item.name + ", ";
-                            })
-                          : null}
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Жанры:</th>
-                      <td>
-                        {genres
-                          ? genres.map(item => {
-                              return item.name + " ";
-                            })
-                          : null}
-                        {/* {genres.toString()} */}
-                      </td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Video activeTab={activeTab} movieId={id} />
-            </Row>
-          </TabPane>
-          <TabPane tabId="3">
-            <Row>
-              <Credits activeTab={activeTab} movieId={id} />
-            </Row>
-          </TabPane>
-        </TabContent>
+        <Switch>
+          <Route path="/movie/:id/detail" component={Detail} />
+          <Route path="/movie/:id/videos" component={Video} />
+          <Route path="/movie/:id/credits" component={Credits} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     );
   }
