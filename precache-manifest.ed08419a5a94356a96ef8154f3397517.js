8c12f7aebe59698a9dd71251a5649462ce29584c
Serhii <babiiserhii88@gmail.com>
2024-11-12 13:41:06 +0200

    Update application

---
+++ precache-manifest.ed08419a5a94356a96ef8154f3397517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd0d4a6e4f8590d7262665794b09bfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f5235141a9be1c5432a",
+    "url": "./static/css/main.87f61177.chunk.css"
+  },
+  {
+    "revision": "8bd7816da5a67750ab5e",
+    "url": "./static/js/2.9c28be8a.chunk.js"
+  },
+  {
+    "revision": "5f5235141a9be1c5432a",
+    "url": "./static/js/main.5c834da6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "./static/js/runtime~main.a8a9905a.js"
+  }
+]);
